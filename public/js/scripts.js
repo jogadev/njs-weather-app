bdde7ccc40650b18c58a450158f85ec298110b43
Jorge Garcia Delgado <jogadev@outlook.com>
2020-04-14 23:33:25 -0500

    Visually displays errors now

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,34 @@
-
-
 document.addEventListener('DOMContentLoaded', (doc, evt) => {
     const cityForm = document.querySelector('#formCity');
+    const resultContainer = document.querySelector('#results');
+    const errorMessageContainer = document.querySelector('#errorMessage');
+
+    // For positive result
+    const temperatureTag = document.querySelector('#results > .row > .col.temp > p')
+    const thermalSensationTag = document.querySelector('#results > .row > .col.thermal-sensation > p')
+    const humidityTag = document.querySelector('#results > .row > .col.humidity > p')
+    const windSpeedTag = document.querySelector('#results > .row > .col.wind-speed > p')
+    const resultHeader = document.querySelector('#results #cityResult')
+
+    // For negative result
+    const errorMessage= document.querySelector("#errorMessage > p");
+    const errorHeader = document.querySelector("#errorMessage > h2");
     cityForm.addEventListener('submit', evt => {
         evt.preventDefault();
-        fetch('/weather?city='+evt.target.city.value).then(response => {
+        fetch('/weather?city=' + evt.target.city.value).then(response => {
             response.json().then(result => {
                 if (result.code != 200) {
-                    console.error("An error", result)
+                    resultContainer.classList.add("hide");
+                    errorMessage.textContent = result.message;
+                    errorHeader.textContent = `Error ${result.code}`;
+                    errorMessageContainer.classList.remove("hide");
                 } else {
-                    const temperatureTag = document.querySelector('#results > .row > .col.temp > p')
-                    const thermalSensationTag = document.querySelector('#results > .row > .col.thermal-sensation > p')
-                    const humidityTag = document.querySelector('#results > .row > .col.humidity > p')
-                    const windSpeedTag = document.querySelector('#results > .row > .col.wind-speed > p')
-
+                    errorMessageContainer.classList.add("hide")
                     temperatureTag.textContent = result.data.temperature.cl;
                     thermalSensationTag.textContent = result.data.thermalSensation.cl;
                     humidityTag.textContent = result.data.humidity;
                     windSpeedTag.textContent = result.data.windSpeed;
-                    const resultHeader = document.querySelector('#results #cityResult')
                     resultHeader.textContent = evt.target.city.value;
-                    const resultContainer = document.querySelector('#results');
                     resultContainer.classList.remove('hide')
                 }
             })
